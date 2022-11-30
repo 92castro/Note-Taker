55b513564f91615f51c5105fdf0462622748d7a1
92castro <celin92castro@gmail.com>
2022-11-29 17:21:36 -0700

    notes append on html file and can be deleted

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const notes = require("./db/db.json");
+const fs = require("fs");
+const uniqid = require("uniqid");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +14,12 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/api/notes", (req, res) => {
+app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
 });
 
 app.post("/api/notes", (req, res) => {
@@ -23,17 +29,32 @@ app.post("/api/notes", (req, res) => {
   if (title && text) {
     const newNote = {
       text,
-      note,
+      title,
+      id: uniqid(),
     };
+
+    notes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2));
 
     const response = {
       status: "success",
       body: newNote,
     };
+    res.json(response);
   }
 });
 
-app.delete("/api/notes/:id", (req, res) => {});
+//splice array method, findIndex to delete note
+app.delete("/api/notes/:id", (req, res) => {
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2));
+  const { id } = req.params;
+  console.log(id + "note id");
+  const noteIndex = notes.findIndex((note) => note.id === id);
+  console.log(noteIndex);
+  const removeNote = notes.splice(noteIndex, 1);
+  return res.send(removeNote);
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
